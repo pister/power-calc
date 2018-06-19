9b58208ba3e0282620f9e67f6fb77f010eb3aa0f
SongLi Huang <zjuhsl@gmail.com>
2018-06-19 19:35:46 +0800

    add os

---
+++ lib/New Group/lib_os.hpp
@@ -0,0 +1,17 @@
+//
+//  os.hpp
+//  power_calc
+//
+//  Created by Songli Huang on 2018/6/19.
+//  Copyright © 2018年 Songli Huang. All rights reserved.
+//
+
+#ifndef lib_os_hpp
+#define lib_os_hpp
+
+#define OS_MODULE_NAME "os"
+
+class XBoot;
+void lib_register_os_module(XBoot &boot);
+
+#endif /* os_hpp */
